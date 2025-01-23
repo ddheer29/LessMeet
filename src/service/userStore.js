25a74fde0831856e07775c7eb14909865e30de59
ddheer29 <divyangdheer11@gmail.com>
2025-01-23 23:13:06 +0530

    user and isLive store

---
+++ src/service/userStore.js
@@ -0,0 +1,10 @@
+import {create} from 'zustand';
+import {persist, createJSONStorage} from 'zustand/middleware';
+import {mmkvStorage} from './storage';
+
+export const useUserStore = create()(
+  persist((set, get) => ({}), {
+    name: 'user-storage',
+    getStorage: createJSONStorage(() => mmkvStorage),
+  }),
+);
