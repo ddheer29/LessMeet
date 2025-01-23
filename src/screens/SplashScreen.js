1cb78fbb4918efa5f52c7427936985208fd68171
ddheer29 <divyangdheer11@gmail.com>
2025-01-23 23:12:45 +0530

    splash screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,14 +1,39 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {resetAndNavigate} from '../utils/NavigationUtils';
+import {screenHeight, screenWidth} from '../utils/Constants';
 
 const SplashScreen = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      resetAndNavigate('HomeScreen');
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('./../assets/images/g.png')}
+        style={styles.image}
+      />
     </View>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: screenWidth * 0.7,
+    height: screenHeight * 0.7,
+    resizeMode: 'contain',
+  },
+});
