fece92cd639e1bc82c591e5fcc5ae6db89281940
ddheer29 <divyangdheer11@gmail.com>
2025-01-24 22:22:49 +0530

    Inquiry Modal developed

---
+++ src/components/home/InquiryModal.js
@@ -0,0 +1,100 @@
+import 'react-native-get-random-values';
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  Modal,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useUserStore} from '../../service/userStore';
+import {v4 as uuidv4} from 'uuid';
+import {inquiryStyles} from '../../styles/inquiryStyles';
+
+const InquiryModal = ({visible, onClose}) => {
+  const {user, setUser} = useUserStore();
+  const [name, setName] = useState('');
+  const [profilePhotoUrl, setProfilePhotoUrl] = useState('');
+
+  const handleSave = () => {
+    if (name && profilePhotoUrl) {
+      setUser({
+        id: uuidv4(),
+        name,
+        photo: profilePhotoUrl,
+      });
+    } else {
+      Alert.alert('Please fill both details');
+    }
+  };
+
+  useEffect(() => {
+    if (visible) {
+      const storedName = user?.name;
+      const storedProfilePhotoUrl = user?.photo;
+      setName(storedName || '');
+      setProfilePhotoUrl(setProfilePhotoUrl || '');
+    }
+  }, [visible]);
+
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      transparent={true}
+      onRequestClose={onClose}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.modalContainer}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={inquiryStyles.keyboardAvoidingView}>
+            <ScrollView contentContainerStyle={inquiryStyles.scrollViewContent}>
+              <View style={inquiryStyles.modalContainer}>
+                <Text style={inquiryStyles.title}>Enter you details</Text>
+                <TextInput
+                  style={inquiryStyles.input}
+                  placeholder="Enter your name"
+                  value={name}
+                  placeholderTextColor={'#ccc'}
+                  onChangeText={setName}
+                />
+                <TextInput
+                  style={inquiryStyles.input}
+                  placeholder="Enter your profile photo url"
+                  value={profilePhotoUrl}
+                  placeholderTextColor={'#ccc'}
+                  onChangeText={setProfilePhotoUrl}
+                />
+                <View style={inquiryStyles.buttonContainer}>
+                  <TouchableOpacity
+                    style={[inquiryStyles.button, inquiryStyles.cancelButton]}
+                    onPress={handleSave}>
+                    <Text style={inquiryStyles.buttonText}>Cancel</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={inquiryStyles.button}
+                    onPress={handleSave}>
+                    <Text style={inquiryStyles.buttonText}>Save</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </ScrollView>
+          </KeyboardAvoidingView>
+        </View>
+      </TouchableWithoutFeedback>
+    </Modal>
+  );
+};
+
+export default InquiryModal;
+
+const styles = StyleSheet.create({
+  modalContainer: {},
+});
